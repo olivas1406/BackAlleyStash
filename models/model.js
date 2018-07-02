3609b38568abfca6dfb23e7002eb2a209160e8c6
unknown <flysparky@cox.net>
2018-07-01 21:53:28 -0700

    playing around with keys

--- models/model.js
+++ models/model.js
@@ -98,7 +98,7 @@ var self = module.exports = {
       user_id: {type: Sequelize.INTEGER, allowNull: false},
       account_type_id: {type: Sequelize.INTEGER, allowNull: false},
       transactions_id: {type: Sequelize.INTEGER, allowNull: false},
-      number: {type: Sequelize.STRING, len: 255, allowNull: false},
+      category_id: {type: Sequelize.INTEGER, allowNull: false},
       amount: {type: Sequelize.FLOAT, allowNull: false},
       balance: {type: Sequelize.FLOAT, allowNull: false}
     },
@@ -119,4 +119,7 @@ self.Office.hasMany(self.Account, {as: 'accounts', foreignKey: 'office_id'});
 self.AccountType.hasMany(self.Account, {as: 'accounts', foreignKey: 'account_type_id'});
 self.Account.belongsTo(self.User, {as: 'user', foreignKey: 'user_id'});
 self.Account.belongsTo(self.Office, {as: 'office', foreignKey: 'office_id'});
-self.Account.belongsTo(self.AccountType, {as: 'account_type', foreignKey: 'account_type_id'});
+self.Account.belongsTo(self.AccountType, {as: 'accounttype', foreignKey: 'account_type_id'});
+self.Transaction.belongsTo(self.Categories, {as: 'categories', foreignKey: 'account_id'});
+
+self.User.hasOne(authentication);
