5ed6c2263b800079dfc872208e713c801e43ee2a
Daniel <olivas1406@msn.com>
2018-07-02 23:55:00 -0700

    Working on Chart on homepage

--- client/src/components/Stash/Stash.js
+++ client/src/components/Stash/Stash.js
@@ -3,10 +3,8 @@ import "./Stash.css";
 
 const Stash = props => (
     <div className="header">{props.children}
-        {/* <div className="chart"> */}
             <canvas id="Stash"></canvas>
         </div>
-    // </div>    
 );
 
 export default Stash;
