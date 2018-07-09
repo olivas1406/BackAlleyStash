785e344ba684922b4af57a4e41c49b28971d75fc
Andrew Nowlin <31021892+AndrewNowlin@users.noreply.github.com>
2018-07-08 21:50:55 -0700

    Add files via upload

---
+++ scripts/seeds.sql
@@ -0,0 +1,15 @@
+use stash
+insert into users (userfirstname, userlastname, email) values ('Trent', 'Cooper', 'trent@trilogy.com'),
+('Inda', 'Anime', 'dragonball@gmail.com'), ('Dan', 'Bon', 'brogrammer1@hotmail.com');
+
+INSERT INTO authentication (login, password, userID) VALUES ('trent23', 'abc123', 1), ('sailormoon', 'anime55', 2), ('javaman', '123abc', 3);
+
+INSERT into accountTypes (accountTypeDesc, interestRate) VALUES ('Checking', 0), ('Savings', 1.0075), ('Money Market', 1.01);
+
+instert into categories (categoryDesc) VALUES ('Auto/Transportation'), ('Bills/Utilities'), ('Business/Office'),
+('Cash/ATM'), ('Credit Card/Loan Payments'), ('Entertainment'), ('Food/Drink'),
+('Health'), ('Home'), ('Insurance/Finacial'), ('Miscellaneous'), ('Outgoing Transfers'),
+('Shopping'), ('Travel'), ('Uncategorized Payments');
+
+
+
