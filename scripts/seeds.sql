111f0c3ef3e77f08d8b5f6e4fef23d36bbd8fac7
Andrew Nowlin <flysparky@cox.net>
2018-07-10 21:06:49 -0700

    fixed seeds

--- scripts/seeds.sql
+++ scripts/seeds.sql
@@ -1,15 +1,60 @@
-use stash
-insert into users (userfirstname, userlastname, email) values ('Trent', 'Cooper', 'trent@trilogy.com'),
-('Inda', 'Anime', 'dragonball@gmail.com'), ('Dan', 'Bon', 'brogrammer1@hotmail.com');
+use stash;
+INSERT into user
+    (firstname, lastname, email, createdAT, UpdatedAt)
+values
+    ('Trent', 'Cooper', 'trent@trilogy.com', NOW(), NOW()),
+    ('Inda', 'Anime', 'dragonball@gmail.com', NOW(), NOW()),
+    ('Dan', 'Bon', 'brogrammer1@hotmail.com', NOW(), NOW());
 
-INSERT INTO authentication (login, password, userID) VALUES ('trent23', 'abc123', 1), ('sailormoon', 'anime55', 2), ('javaman', '123abc', 3);
+INSERT INTO authentication
+    (login, password, userID, createdAT, UpdatedAt)
+VALUES
+    ('trent23', 'abc123', 1, NOW(), NOW()),
+    ('sailormoon', 'anime55', 2, NOW(), NOW()),
+    ('javaman', '123abc', 3, NOW(), NOW());
 
-INSERT into accountTypes (accountTypeDesc, interestRate) VALUES ('Checking', 0), ('Savings', 1.0075), ('Money Market', 1.01);
+INSERT into accountType
+    (accountTypeDesc, interestRate, createdAt, UpdatedAT)
+VALUES
+    ('Checking', 0, NOW(), NOW()),
+    ('Savings', 1.0075, NOW(), NOW()),
+    ('Money Market', 1.01, NOW(), NOW());
 
-instert into categories (categoryDesc) VALUES ('Auto/Transportation'), ('Bills/Utilities'), ('Business/Office'),
-('Cash/ATM'), ('Credit Card/Loan Payments'), ('Entertainment'), ('Food/Drink'),
-('Health'), ('Home'), ('Insurance/Finacial'), ('Miscellaneous'), ('Outgoing Transfers'),
-('Shopping'), ('Travel'), ('Uncategorized Payments');
+insert into categories
+    (categoryDesc, CreatedAt, UpdatedAT)
+VALUES
+    ('Auto/Transportation', NOW(), NOW()),
+    ('Bills/Utilities', NOW(), NOW()),
+    ('Business/Office', NOW(), NOW()),
+    ('Cash/ATM', NOW(), NOW()),
+    ('Credit Card/Loan Payments', NOW(), NOW()),
+    ('Entertainment', NOW(), NOW()),
+    ('Food/Drink', NOW(), NOW()),
+    ('Health', NOW(), NOW()),
+    ('Home', NOW(), NOW()),
+    ('Insurance/Finacial', NOW(), NOW()),
+    ('Miscellaneous', NOW(), NOW()),
+    ('Outgoing Transfers', NOW(), NOW()),
+    ('Shopping', NOW(), NOW()),
+    ('Travel', NOW(), NOW()),
+    ('Uncategorized Payments', NOW(), NOW());
 
+
+
+INSERT INTO account
+    (UserID, accountTypeDesc, transactionDesc, CategoryDesc, Amount, Balance, CreatedAT, UpdatedAt,
+    accountTypeID)
+VALUES
+    (2, 'Checking', 'anime posters from comic store', 'Shopping', 40.44, 100.00, NOW(), NOW(),
+        1),
+    (1, 'Checking', 'Wells Fargo AutoLoan', 'Credit Card/Loan Payments', 599, 10000.00, NOW(), NOW(),
+        1);
+
+/*This has issues due to key constraints*/
+INSERT INTO transaction
+    (transactiondesc, categorydesc, amount, balance, createdAT, UpdatedAT, accountTypeID, CategoryID, UserID, usersID)
+VALUES
+    ('anime posters from comic store', 'Shopping', 40.44, 100.00, NOW(), NOW(), 1, 12, 2, 2),
+    ('Wells Fargo AutoLoan', 'Credit Card/Loan Payments', 599.00, 10000.00, NOW(), NOW(), 1, 5, 1, 1);
 
 
