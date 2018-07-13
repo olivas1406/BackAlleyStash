6102cdcaeb980ac80caa80d97455cdf80ebe6ca6
Daniel <olivas1406@msn.com>
2018-07-12 21:46:19 -0700

    Added Firebase Authentication, removed 'pages'

---
+++ client/src/components/StockAPI/StockAPI.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./StockAPI.css";
+
+const StockAPI = () => (
+  <div className="Stocks">
+    <h2 className="ExpenseText">Stocks</h2>
+    <hr />
+    <br />
+    <table className="Stocks">
+        <tbody>
+            <tr>
+                <th>Stock</th>
+                <th>Ticker Symbol</th>
+                <th>Change</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Test</td>
+                <td>TST</td>
+                <td>+</td>
+                <td>$1.00</td>
+            </tr>
+            <tr>
+                <td>Test</td>
+                <td>TST</td>
+                <td>+</td>
+                <td>$1.00</td>
+            </tr>
+            <tr>
+                <td>Test</td>
+                <td>TST</td>
+                <td>+</td>
+                <td>$1.00</td>
+            </tr>
+            <tr>
+                <td>Test</td>
+                <td>TST</td>
+                <td>+</td>
+                <td>$1.00</td>
+            </tr>
+            <tr>
+                <td>Test</td>
+                <td>TST</td>
+                <td>+</td>
+                <td>$1.00</td>
+            </tr>
+        </tbody>
+    </table>
+    <br />
+  </div>
+);
+
+export default StockAPI;
