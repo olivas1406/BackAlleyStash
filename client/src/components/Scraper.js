5e2985a2e7f535a1e2f03f51cbf119fcd7cb2602
Daniel <olivas1406@msn.com>
2018-07-17 07:24:31 -0700

    Added headline scraper functionality to Scraper.js

--- client/src/components/Scraper.js
+++ client/src/components/Scraper.js
@@ -1,42 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import "../style.css";
 
-const Scraper = () => (
-  <div className="Scraper">
-    <h2 className="ExpenseText">Financial Headlines</h2>
-    <hr />
-    <br />
-    <table className="StocksTable">
-      <tbody>
-        <tr>
-          <th>Headline</th>
-          <th>Summary</th>
-          <th>Link to Article</th>
-        </tr>
-        <tr>
-          <td>Dow clears 25K as the Nasdaq sits at a record high</td>
-          <td>U.S. stocks wrapped a winning week on Friday with the Dow Jones Industrial Average closing above the psychologically significant 25,000-point milestone</td>
-          <a href="_blank"><td>Click Me</td></a>
-        </tr>
-        <tr>
-          <td>Fed emphasizes 'solid' US economic growth, repeats gradual approach</td>
-          <td>The Fed has raised interest rates seven times since it began a tightening cycle back in December 2015 and last lifted its benchmark lending rate by a quarter percentage point in mid June.</td>
-          <a href="_blank"><td>Click Me</td></a>
-        </tr>
-        <tr>
-          <td>Papa John’s CEO Attempts Damage Control But Not An Apology</td>
-          <td>Papa John’s CEO Steve Ritchie had one job on Friday: Do damage control for the company after reports surfaced this week that founder John Schnatter used a racial slur in a conference call in May.</td>
-          <a href="_blank"><td>Click Me</td></a>
-        </tr>
-        <tr>
-          <td>Citigroup Earnings Lifted by Growing Loans</td>
-          <td>New York bank’s trading revenue falls about 1% in latest quarter; per-share earnings beat analysts’ expectations</td>
-          <a href="_blank"><td>Click Me</td></a>
-        </tr>
-        </tbody>
-    </table>
-    <br />
-  </div>
-);
+class Scraper extends Component {
+    constructor() {
+        super();
+        this.state = {
+            scraper: [], 
+        };
+    }
+
+  componentDidMount() {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=5&apiKey=929c659d639b40d4a37427546431b819')
+    .then(results => {
+        console.log(`results:`, results);
+        return results.json();
+    }).then(data => {
+        let scraper = data.articles.map((scrapeMe) => {
+            return(
+                    <tr key={scrapeMe.articles}>
+                    <td>{scrapeMe.title}</td>
+                    <td>{scrapeMe.description}</td>
+                    <td><button><a href={scrapeMe.url} target="_blank">Click Me</a></button></td>
+                    </tr>
+            )
+        })
+        this.setState({scraper: scraper});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="Scraper">
+          <h2 className="ExpenseText">Financial Headlines</h2>
+          <hr />
+          <br />
+          <table className="StocksTable">
+            <tbody>
+              <tr>
+                <th>Headline</th>
+                <th>Summary</th>
+                <th>Link to Article</th>
+              </tr>
+              {this.state.scraper}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Scraper;
