f61e4f7559f5f97b9e5a0bf07c8b0060a953b33b
Bon Erick Silvallana <erick_malas95@hotmail.com>
2018-07-17 18:49:43 -0700

    adding form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import withAuthorization from './withAuthorization';
-import { db } from '../firebase';
+import React, { Component } from "react";
+import withAuthorization from "./withAuthorization";
+import { db } from "../firebase";
 import Header from "./Header.js";
 import Savings from "./Savings.js";
 import Assets from "./Assets.js";
 import StockAPI from "./StockAPI.js";
 import Scraper from "./Scraper.js";
-import Expenses from './Expenses.js';
+import Expenses from "./Expenses.js";
+import Form from "./Form.js";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      users: null,
+      users: null
     };
   }
 
@@ -24,20 +25,19 @@ class HomePage extends Component {
   }
 
   render() {
-
     return (
       <div className="App">
-      <Header />
-      <Expenses />  
-      <Savings />
-      <Assets />
-      <StockAPI />
-      <Scraper />
+        <Header />
+        <Expenses />
+        <Savings />
+        <Assets />
+        <StockAPI />
+        <Scraper />
       </div>
     );
   }
 }
-  
-const authCondition = (authUser) => !!authUser;
+
+const authCondition = authUser => !!authUser;
 
 export default withAuthorization(authCondition)(HomePage);
