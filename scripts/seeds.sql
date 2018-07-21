b826f697cf35acff628b1d4535a438997df3c88f
Bon Erick Silvallana <erick_malas95@hotmail.com>
2018-07-21 14:37:08 -0700

    removed schema.sql

--- scripts/seeds.sql
+++ scripts/seeds.sql
@@ -1,54 +1,53 @@
-USE stash;
+use stash;
 
-INSERT INTO user
+
+INSERT into user
     (firstname, lastname, email, createdAT, UpdatedAt)
-VALUES
+values
     ('Trent', 'Cooper', 'trent@trilogy.com', NOW(), NOW()),
-    ('Daniel', 'Olivas', 'me@danielolivas.net', NOW(), NOW()),
-    ('Dan', 'Shook', 'shook.dan@gmail.com', NOW(), NOW()),
-    ('Andrew', 'Nowlin', 'flyspark@cox.net', NOW(), NOW()),
-    ('Bon', 'Silvallana', 'bon@idontknowhisemail.com', NOW(), NOW());
+    ('Inda', 'Anime', 'dragonball@gmail.com', NOW(), NOW()),
+    ('Dan', 'Bon', 'brogrammer1@hotmail.com', NOW(), NOW());
 
-INSERT INTO accountType
+INSERT INTO authentication
+    (login, password, userID, createdAT, UpdatedAt)
+VALUES
+    ('trent23', 'abc123', 1, NOW(), NOW()),
+    ('sailormoon', 'anime55', 2, NOW(), NOW()),
+    ('javaman', '123abc', 3, NOW(), NOW());
+
+INSERT into accountType
     (accountTypeDesc, interestRate, createdAt, UpdatedAT)
 VALUES
     ('Checking', 0, NOW(), NOW()),
     ('Savings', 1.0075, NOW(), NOW()),
     ('Money Market', 1.01, NOW(), NOW());
 
-INSERT INTO categories
+insert into categories
     (categoryDesc, CreatedAt, UpdatedAT)
 VALUES
     ('Auto/Transportation', NOW(), NOW()),
-    ('Utilities', NOW(), NOW()),
-    ('Business Expense', NOW(), NOW()),
-    ('Cash Withdrawal', NOW(), NOW()),
+    ('Bills/Utilities', NOW(), NOW()),
+    ('Business/Office', NOW(), NOW()),
+    ('Cash/ATM', NOW(), NOW()),
     ('Credit Card/Loan Payments', NOW(), NOW()),
     ('Entertainment', NOW(), NOW()),
     ('Food/Drink', NOW(), NOW()),
     ('Health', NOW(), NOW()),
     ('Home', NOW(), NOW()),
-    ('Insurance', NOW(), NOW()),
+    ('Insurance/Finacial', NOW(), NOW()),
     ('Miscellaneous', NOW(), NOW()),
     ('Outgoing Transfers', NOW(), NOW()),
     ('Shopping', NOW(), NOW()),
     ('Travel', NOW(), NOW()),
-    ('Emergency Expense', NOW(), NOW());
--- ('Uncategorized Payments', NOW(), NOW());  'Uncategorized' categorizes it. - DLO
-
--- INSERT INTO account
---     (UserID, accountTypeDesc, transactionDesc, CategoryDesc, Amount, Balance, CreatedAT, UpdatedAt,
---     accountTypeID)
--- VALUES
---     (2, 'Checking', 'Mortgage Payment', 'Home', 1200, 160000, NOW(), NOW(), 1),
---     (2, 'Checking', 'Salt River Project', 'Utilities', 350, 0, NOW(), NOW(), 1),
---     (2, 'Checking', 'City of Phoenix', 'Utilities', 80, 0, NOW(), NOW(), 1),
---     (1, 'Checking', 'Wells Fargo AutoLoan', 'Credit Card/Loan Payments', 300, 10000, NOW(), NOW(), 1),
---     (2, 'Checking', 'Verizon Wireless', 'Utilities', 220, 0, NOW(), NOW(), 1),
---     (2, 'Checking', 'Cox Communication', 'Utilities', 120, 0, NOW(), NOW(), 1),
---     (2, 'Checking', 'Breakfast w/ Bon @ IHOP', 'Food/Drink', 25, 0, NOW(), NOW(), 1),
---     (1, 'Checking', 'Emergency Room (Food Poisoning)', 'Emergency Expense', 400, 0, NOW(), NOW(), 1);
-    -- (2, 'Checking', 'Rx', 'Health', 12, 0, NOW(), NOW(), 1),
-    -- (1, 'Checking', 'Underwear', 'Emergency Expense', 10, 0, NOW(), NOW(), 1);
+    ('Uncategorized Payments', NOW(), NOW());
 
 
+
+INSERT INTO account
+    (UserID, accountTypeDesc, transactionDesc, CategoryDesc, Amount, Balance, CreatedAT, UpdatedAt,
+    accountTypeID)
+VALUES
+    (2, 'Checking', 'anime posters from comic store', 'Shopping', 40.44, 100.00, NOW(), NOW(),
+        1),
+    (1, 'Checking', 'Wells Fargo AutoLoan', 'Credit Card/Loan Payments', 599, 10000.00, NOW(), NOW(),
+        1);
